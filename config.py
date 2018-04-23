eadccc4e06cda611d2285b09b04108d613598d54
DavidNganga <ngashiedavid@gmail.com>
2018-04-23 15:36:38 +0300

    add nothing

--- config.py
+++ config.py
@@ -6,9 +6,6 @@ class Config:
     
 class ProdConfig(Config):
    SQLALCHEMY_DATABASE_URIN= os.environ.get('DATABASE_URL')
-
-   DEBUG = True
-
 class DevConfig(Config):
     
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://david:somanypasswords@localhost/blog'
